c7cef0e83bff480e6394afacd6360b18a3c7d778
Cal <calpnicklin@gmail.com>
2022-12-02 12:36:55 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+);
+
+console.log('Hello Node.js project.');
+
+console.log(process.env.MY_SECRET);
