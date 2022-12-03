941e60c51dd4549c6a2c7b9e0c838f2b3813eaf0
Cal <calpnicklin@gmail.com>
2022-12-03 20:18:28 +0000

    connects api to sequelize

---
+++ src/models/index.js
@@ -0,0 +1,28 @@
+import Sequelize from 'sequelize';
+
+import getUserModel from './user';
+import getMessageModel from './message';
+
+const sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        dialect: 'postgres',
+    },
+);
+
+const models = {
+    User: getUserModel(sequelize, Sequelize),
+    Message: getMessageModel(sequelize, Sequelize),
+};
+
+Object.keys(models).forEach((key) => {
+    if ('associate' in models[key]) {
+        models[key].associate(models);
+    }
+});
+
+export { sequelize };
+
+export default models;
